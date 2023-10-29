e0b6aafbd211f21a9ffa167a223e544a5dfd8c8d
jalilian_Armin <armin.jalilian@live.com>
2023-10-29 11:33:15 +0330

    only dailywarehouse

--- warehouse.py
+++ warehouse.py
@@ -21,10 +21,10 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
         # Add your compute logic for the report page here
         
         self.addPartBtn.clicked.connect(self.savetoDB)
-        self.exitPartBtn.clicked.connect(self.move_record)
-        self.exitPartSerial.editingFinished.connect(self.on_text_changed)
+        # self.exitPartBtn.clicked.connect(self.move_record)
+        # self.exitPartSerial.editingFinished.connect(self.on_text_changed)
         self.refreshWarehouse.clicked.connect(self.load_data)
-        self.refreshDailyWarehouse.clicked.connect(self.load_datadaily)
+        # self.refreshDailyWarehouse.clicked.connect(self.load_datadaily)
 
 
     def savetoDB(self):
@@ -72,7 +72,7 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
 
 
         # Define the SQL query to insert data into the table
-        insert_part = "INSERT INTO warehouse (orderdate, category, serial, ordernum, supplier, company, country, operator, currentdate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        insert_part = "INSERT INTO dailywarehouse (orderdate, category, serial, ordernum, supplier, company, country, operator, currentdate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
     
         
@@ -96,7 +96,7 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
         cursor = connection.cursor()
 
         # Fetch data from the database
-        cursor.execute("SELECT * FROM warehouse")
+        cursor.execute("SELECT * FROM dailywarehouse")
         data = cursor.fetchall()
 
         # Populate the QTableWidget
@@ -121,7 +121,7 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
         cursor = connection.cursor()
 
         # Delete the record with the specified ID
-        cursor.execute("DELETE FROM warehouse WHERE id=%s", (record_id,))
+        cursor.execute("DELETE FROM dailywarehouse WHERE id=%s", (record_id,))
         connection.commit()
 
         # Reload data after deletion
@@ -135,46 +135,46 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
 
 ######################################tab exit################################
 
-    def load_datadaily(self):
-        # Connect to the MySQL database
-        connection =self.database_info()
+    # def load_datadaily(self):
+    #     # Connect to the MySQL database
+    #     connection =self.database_info()
 
-        cursor = connection.cursor()
+    #     cursor = connection.cursor()
 
-        # Fetch data from the database
-        cursor.execute("SELECT * FROM dailywarehouse")
-        data = cursor.fetchall()
+    #     # Fetch data from the database
+    #     cursor.execute("SELECT * FROM dailywarehouse")
+    #     data = cursor.fetchall()
 
-        # Populate the QTableWidget
-        self.tableWidget_2.setRowCount(len(data))
-        for row_index, row_data in enumerate(data):
-            for col_index, cell_data in enumerate(row_data):
-                if col_index == 0:  # Create delete button in the last column
-                    delete_button = QPushButton("Delete")
-                    delete_button.clicked.connect(lambda _, r=row_data[0]: self.delete_recorddaily(connection, r))
-                    self.tableWidget_2.setCellWidget(row_index, col_index, delete_button)
-                else:
-                    item = QTableWidgetItem(str(cell_data))
-                    self.tableWidget_2.setItem(row_index, col_index, item)
+    #     # Populate the QTableWidget
+    #     self.tableWidget_2.setRowCount(len(data))
+    #     for row_index, row_data in enumerate(data):
+    #         for col_index, cell_data in enumerate(row_data):
+    #             if col_index == 0:  # Create delete button in the last column
+    #                 delete_button = QPushButton("Delete")
+    #                 delete_button.clicked.connect(lambda _, r=row_data[0]: self.delete_recorddaily(connection, r))
+    #                 self.tableWidget_2.setCellWidget(row_index, col_index, delete_button)
+    #             else:
+    #                 item = QTableWidgetItem(str(cell_data))
+    #                 self.tableWidget_2.setItem(row_index, col_index, item)
 
-        # Close the database connection
-        cursor.close()
-        connection.close()
+    #     # Close the database connection
+    #     cursor.close()
+    #     connection.close()
 
-    def delete_recorddaily(self, connection, record_id):
-        connection = self.database_info()
+    # def delete_recorddaily(self, connection, record_id):
+    #     connection = self.database_info()
 
-        cursor = connection.cursor()
+    #     cursor = connection.cursor()
 
-        # Delete the record with the specified ID
-        cursor.execute("DELETE FROM dailywarehouse WHERE id=%s", (record_id,))
-        connection.commit()
+    #     # Delete the record with the specified ID
+    #     cursor.execute("DELETE FROM dailywarehouse WHERE id=%s", (record_id,))
+    #     connection.commit()
 
-        # Reload data after deletion
-        self.load_datadaily()
+    #     # Reload data after deletion
+    #     self.load_datadaily()
 
-        # Close the cursor (not the connection)
-        cursor.close()
+    #     # Close the cursor (not the connection)
+    #     cursor.close()
 
 
 
@@ -182,50 +182,50 @@ class WarehousePage(QMainWindow, Ui_WarehousePage):
 
 
              
-    def on_text_changed(self):
-        serial = self.exitPartSerial.text().strip()  # Get rid of any leading/trailing spaces
-        db_connection = self.database_info()
+    # def on_text_changed(self):
+    #     serial = self.exitPartSerial.text().strip()  # Get rid of any leading/trailing spaces
+    #     db_connection = self.database_info()
 
-        cursor = db_connection.cursor()
-        query = "SELECT category FROM warehouse WHERE serial = %s"
+    #     cursor = db_connection.cursor()
+    #     query = "SELECT category FROM warehouse WHERE serial = %s"
         
-        cursor.execute(query, (serial,))
-        result = cursor.fetchone()
-        self.exitPartName.setText(str(result))
-        print("Text changed:", serial)
-        # Your custom code here
+    #     cursor.execute(query, (serial,))
+    #     result = cursor.fetchone()
+    #     self.exitPartName.setText(str(result))
+    #     print("Text changed:", serial)
+    #     # Your custom code here
 
 
 
 
-    def move_record(self):
-        try:
-            # Connect to the MySQL database
-            db_connection = self.database_info()
+    # def move_record(self):
+    #     try:
+    #         # Connect to the MySQL database
+    #         db_connection = self.database_info()
 
-            # Create a cursor
-            cursor = db_connection.cursor()
+    #         # Create a cursor
+    #         cursor = db_connection.cursor()
 
-            # Specify the row number to move
-            serial = self.exitPartSerial.text().strip()
+    #         # Specify the row number to move
+    #         serial = self.exitPartSerial.text().strip()
 
-            # Move the row from table1 to table2
-            query_move = f"INSERT INTO dailywarehouse SELECT * FROM warehouse WHERE serial = '{serial}'"
-            cursor.execute(query_move)
-            db_connection.commit()
+    #         # Move the row from table1 to table2
+    #         query_move = f"INSERT INTO dailywarehouse SELECT * FROM warehouse WHERE serial = '{serial}'"
+    #         cursor.execute(query_move)
+    #         db_connection.commit()
 
-            # Delete the row from table1
-            query_delete = f"DELETE FROM warehouse WHERE serial = '{serial}'"
-            cursor.execute(query_delete)
-            db_connection.commit()
+    #         # Delete the row from table1
+    #         query_delete = f"DELETE FROM warehouse WHERE serial = '{serial}'"
+    #         cursor.execute(query_delete)
+    #         db_connection.commit()
 
-            # Close the cursor and connection
-            cursor.close()
-            db_connection.close()
-            self.load_datadaily()
+    #         # Close the cursor and connection
+    #         cursor.close()
+    #         db_connection.close()
+    #         self.load_datadaily()
 
-        except mysql.connector.Error as err:
-            print("Error:", err)
+    #     except mysql.connector.Error as err:
+    #         print("Error:", err)
 
 
 
